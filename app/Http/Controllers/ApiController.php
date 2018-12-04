9d5154b19d03c5e42cbb652d3832e70e19a58813
Luis Blanco <luedblra@gmail.com>
2018-12-04 08:34:59 -0400

    se ajusta la consulta al procedimientos de base datos para API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,25 +11,42 @@ use Illuminate\Support\Facades\DB;
 
 class ApiController extends Controller
 {
-   public function AllExpecifict($carrier,$origin,$destination){
-      $origin        = Harbor::where('name',$origin)->orWhere('id',$origin)->first();
-      $destination   = Harbor::where('name',$destination)->orWhere('id',$destination)->first();
-      $carrier       = Carrier::where('name',$carrier)->orWhere('id',$carrier)->first();
-      $schedules     = DB::select('call procedure_schedules_all('.$origin['id'].','.$destination['id'].','.$carrier['id'].')');
-      dd($schedules);
-      return response()->json($schedules);
-   }
+    public function AllExpecifict($carrier,$origin,$destination){
+        $originBol      = false;
+        $destinationBol = false;
+        $carrierBol     = false;
 
-   public function ForCarrier($carrier){
+        $origin        = Harbor::where('name',$origin)->orWhere('id',$origin)->first();
+        $destination   = Harbor::where('name',$destination)->orWhere('id',$destination)->first();
+        $carrier       = Carrier::where('name',$carrier)->orWhere('id',$carrier)->first();
 
-      $carrier    = Carrier::where('name',$carrier)->orWhere('id',$carrier)->first();
-      if(count($carrier) > 0){
+        if(count($origin) == 1){
+            $originBol  = true;
+        }
+        if(count($destination) == 1){
+            $destinationBol = true;
+        }
+        if(count($carrier) == 1){
+            $carrierBol = true;
+        }
 
-         $schedules  = DB::select('call procedure_schedules('.$carrier["id"].')');
-         return response()->json($schedules);
-      } else {
-         $schedules = [];
-         return response()->json($schedules);
-      }
-   }
+        if($originBol == true && $destinationBol == true && $carrierBol == true ){
+
+            $schedules     = DB::select('call procedure_schedules_all('.$origin['id'].','.$destination['id'].','.$carrier['id'].')');
+        } else {
+            $schedules = [];
+        }
+        return response()->json($schedules);
+    }
+
+    public function ForCarrier($carrier){
+
+        $carrier    = Carrier::where('name',$carrier)->orWhere('id',$carrier)->first();
+        if(count($carrier) > 0){
+            $schedules  = DB::select('call procedure_schedules('.$carrier["id"].')');
+        } else {
+            $schedules = [];
+        }
+        return response()->json($schedules);
+    }
 }
