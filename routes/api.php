47d62b9ff27dc611984458a4570f80dcda574285
Luis Blanco <luedblra@gmail.com>
2018-12-04 11:16:48 -0400

    se agregan algunas configuraciones de api token, aun en desarrollo

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware('auth:api')->prefix('scheduleAPI')->group(function () {
+
+Route::group(['middleware' => 'auth:api'], function() {
    Route::get('/{carrier}/{origin}/{destination}','ApiController@AllExpecifict');
    Route::get('/{carrier}','ApiController@ForCarrier')->name('for.carrier');
 });
+
+
+//https://medium.com/@cvallejo/sistema-de-autenticaci%C3%B3n-api-rest-con-laravel-5-6-240be1f3fc7d
