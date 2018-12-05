0920818c43bcc7c9b45e5b4a28692b793dd32f95
Luis Blanco <luedblra@gmail.com>
2018-12-05 15:51:23 -0400

    tiempo de expiracion 4 dias

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,24 +8,26 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
+   /**
      * The policy mappings for the application.
      *
      * @var array
      */
-    protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-    ];
+   protected $policies = [
+      'App\Model' => 'App\Policies\ModelPolicy',
+   ];
 
-    /**
+   /**
      * Register any authentication / authorization services.
      *
      * @return void
      */
-    public function boot()
-    {
-        $this->registerPolicies();
+   public function boot()
+   {
+      $this->registerPolicies();
 
-        Passport::routes();
-    }
+      Passport::routes();
+      
+      Passport::tokensExpireIn(now()->addDays(4));
+   }
 }
