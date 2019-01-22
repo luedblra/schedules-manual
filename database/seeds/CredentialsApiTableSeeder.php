d3d8e01820e1f393cbb91bf82e351917cca67abc
Luis <luedblra@gmail.com>
2019-01-22 13:31:43 -0400

    se actualiza seed

--- database/seeds/CredentialsApiTableSeeder.php
+++ database/seeds/CredentialsApiTableSeeder.php
@@ -27,7 +27,23 @@ class CredentialsApiTableSeeder extends Seeder
                 'password' => 'secret',
                 'url' => 'schedule/maersk',
                 'carrier_id' => 12,
-                'description' => 'Sutomatic Login',
+                'description' => '	
+Login Sautomatic Maersk',
+                'created_at' => NULL,
+                'updated_at' => '2019-01-22 13:47:47',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'auth_post' => 'http://sautomatic/',
+                'client_id' => '2',
+                'client_secret' => '4A8kVmi5V8U9wawEHTcrF4AWMf7sRxpUFVvkWrPE',
+                'user_name' => 'user@example.com',
+                'password' => 'secret',
+                'url' => 'schedule/cosco',
+                'carrier_id' => 4,
+                'description' => 'Login Sautomatic Cosco
+',
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
